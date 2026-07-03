d2f1d6f2cb92630be99c9f57810519d4c586e3f3
MOHAMED-RIYAS-S <mdriya0101@gmail.com>
2026-07-03 10:43:44 +0530

    Add files via upload

---
+++ day4/Sample.js
@@ -0,0 +1,11 @@
+A=24;
+B=62;
+C=A+B;
+D=86;
+
+console.log("the value of a is",a);
+console.log("the value of B is",B);
+console.log("the value of C is",C);
+console.log(C == 86 && );
+console.log(C == 86 || D == 86);
+console.log(typeof A);
